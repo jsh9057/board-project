d67b1f1a75008d4f7d6ae9879a7667545f18f5e6
jsh9057 <jsh9057@skuniv.ac.kr>
2023-05-23 20:05:02 +0900

    #1 - Querydsl 지원을 이용하여 api 검색 옵션 추가
    `QuerydslPredicateExecutor`,
    `QuerydslBinderCustomizer` 를 이용하여 간단화

--- src/main/java/com/jsh/boardproject/repository/ArticleCommentRepository.java
+++ src/main/java/com/jsh/boardproject/repository/ArticleCommentRepository.java
@@ -1,9 +1,30 @@
 package com.jsh.boardproject.repository;
 
 import com.jsh.boardproject.domain.ArticleComment;
+import com.jsh.boardproject.domain.QArticle;
+import com.jsh.boardproject.domain.QArticleComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository
+        extends JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<ArticleComment>,
+        QuerydslBinderCustomizer<QArticleComment> {
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticleComment root) {  // 추가 기능을 만들기위해 커스터마이즈를 구현을 사용
+        bindings.excludeUnlistedProperties(true); // 현재 QuerydslPredicateExecutor 때문에 Article 에 대한 모든 검색이 열려있음.
+        // 하지만 부분적인 검색을 원하기 때문에 필요없는건 검색에서 제외하려함
+        bindings.including(root.content, root.createdAt, root.createdBy); // 검색에 사용할 요소를 추가
+//        bindings.bind(root.title).first(StringExpression::likeIgnoreCase);  // like '${v}'
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
