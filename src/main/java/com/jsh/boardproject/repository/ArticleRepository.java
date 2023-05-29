6c50c5020bdf6df3cfdd03c3433c3e7da65c448b
jsh9057 <jsh9057@skuniv.ac.kr>
2023-05-29 17:20:32 +0900

    #21 - 비즈니스 로직 구현
    테스트를 토대로 게시판 서비스에 필요한 비즈니스 로직 구현 완료

--- src/main/java/com/jsh/boardproject/repository/ArticleRepository.java
+++ src/main/java/com/jsh/boardproject/repository/ArticleRepository.java
@@ -18,14 +18,17 @@ public interface ArticleRepository extends
         QuerydslPredicateExecutor<Article>,     // 엔티티 안에있는 모든 필드에 대한 기본 검색기능을 추가해 줌
         QuerydslBinderCustomizer<QArticle> {
 
-    Page<Article> findByTitle(String title, Pageable pageable);
+    Page<Article> findByTitleContaining(String title, Pageable pageable);
+    Page<Article> findByContentContaining(String content, Pageable pageable);
+    Page<Article> findByUserAccount_UserIdContaining(String userId, Pageable pageable);
+    Page<Article> findByUserAccount_NicknameContaining(String nickname, Pageable pageable);
+    Page<Article> findByHashtag(String hashtag, Pageable pageable);
 
     @Override
     default void customize(QuerydslBindings bindings, QArticle root) {  // 추가 기능을 만들기위해 커스터마이즈를 구현을 사용
         bindings.excludeUnlistedProperties(true); // 현재 QuerydslPredicateExecutor 때문에 Article 에 대한 모든 검색이 열려있음.
                                                   // 하지만 부분적인 검색을 원하기 때문에 필요없는건 검색에서 제외하려함
         bindings.including(root.title, root.content ,root.hashtag, root.createdAt, root.createdBy); // 검색에 사용할 요소를 추가
-//        bindings.bind(root.title).first(StringExpression::likeIgnoreCase);  // like '${v}'
         bindings.bind(root.title).first(StringExpression::containsIgnoreCase); // like '%${v}%' , 대소문자 무시
         bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
         bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
