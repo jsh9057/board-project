b1d20faccc71c89ef83bda995d52e8a851f211d0
jsh9057 <jsh9057@skuniv.ac.kr>
2023-06-20 21:50:13 +0900

    #37 - 게시판, 게시글 뷰의 기능에 대응하는 자바 코드 구현

--- src/test/java/com/jsh/boardproject/service/ArticleServiceTest.java
+++ src/test/java/com/jsh/boardproject/service/ArticleServiceTest.java
@@ -2,11 +2,11 @@ package com.jsh.boardproject.service;
 
 import com.jsh.boardproject.domain.Article;
 import com.jsh.boardproject.domain.UserAccount;
-import com.jsh.boardproject.domain.type.SearchType;
+import com.jsh.boardproject.domain.constant.SearchType;
 import com.jsh.boardproject.dto.ArticleDto;
-import com.jsh.boardproject.dto.ArticleWithCommentsDto;
 import com.jsh.boardproject.dto.UserAccountDto;
 import com.jsh.boardproject.repository.ArticleRepository;
+import com.jsh.boardproject.repository.UserAccountRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,6 +15,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import javax.persistence.EntityNotFoundException;
 import java.time.LocalDateTime;
@@ -33,6 +34,7 @@ class ArticleServiceTest {
     @InjectMocks private ArticleService sut;
 
     @Mock private ArticleRepository articleRepository;
+    @Mock private UserAccountRepository userAccountRepository;
 
     @DisplayName("검색어 없이 게시글을 검색하면, 게시글 페이지를 반환한다.")
     @Test
@@ -104,7 +106,7 @@ class ArticleServiceTest {
         given(articleRepository.findById(articleId)).willReturn(Optional.of(article));
 
         // When
-        ArticleWithCommentsDto dto = sut.getArticle(articleId);
+        ArticleDto dto = sut.getArticle(articleId);
 
         // Then
         assertThat(dto)
@@ -114,14 +116,14 @@ class ArticleServiceTest {
         then(articleRepository).should().findById(articleId);
     }
 
-    @DisplayName("없는 게시글을 조회하면, 예외를 던진다.")
+    @DisplayName("게시글이 없으면, 예외를 던진다.")
     @Test
     void givenNonexistentArticleId_whenSearchingArticle_thenThrowsException() {
         // Given
         Long articleId = 0L;
 
         // When
-        Throwable t = catchThrowable(()-> sut.getArticle(articleId));
+        Throwable t = catchThrowable(()-> sut.getArticleWithComments(articleId));
 
         // Then
         assertThat(t)
@@ -135,12 +137,14 @@ class ArticleServiceTest {
     void givenArticleInfo_whenSavingArticle_thenSavesArticle() {
         // Given
         ArticleDto dto = createArticleDto();
+        given(userAccountRepository.getReferenceById(dto.userAccountDto().userId())).willReturn(createUserAccount());
         given(articleRepository.save(any(Article.class))).willReturn(createArticle());
 
         // When
         sut.saveArticle(dto);
 
         // Then
+        then(userAccountRepository).should().getReferenceById(dto.userAccountDto().userId());
         then(articleRepository).should().save(any(Article.class));
     }
 
@@ -153,7 +157,7 @@ class ArticleServiceTest {
         given(articleRepository.getReferenceById(dto.id())).willReturn(article);
 
         // When
-        sut.updateArticle(dto);
+        sut.updateArticle(dto.id(),dto);
 
         // Then
         assertThat(article)
@@ -171,7 +175,7 @@ class ArticleServiceTest {
         given(articleRepository.getReferenceById(dto.id())).willThrow(EntityNotFoundException.class);
 
         // When
-        sut.updateArticle(dto);
+        sut.updateArticle(dto.id(),dto);
 
         // Then
         then(articleRepository).should().getReferenceById(dto.id());
@@ -232,12 +236,15 @@ class ArticleServiceTest {
     }
 
     private Article createArticle(){
-        return Article.of(
+        Article article =  Article.of(
                 createUserAccount(),
                 "title",
                 "content",
                 "#java"
         );
+        ReflectionTestUtils.setField(article,"id",1L);
+
+        return article;
     }
 
     private ArticleDto createArticleDto(){
@@ -260,7 +267,6 @@ class ArticleServiceTest {
 
     private UserAccountDto createUserAccountDto() {
         return UserAccountDto.of(
-                1L,
                 "jsh",
                 "password",
                 "jsh@mail.com",
