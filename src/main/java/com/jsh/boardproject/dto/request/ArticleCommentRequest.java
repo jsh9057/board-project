0b69acbbd271a9c129fa67c27ee1b331164ef27f
jsh9057 <jsh9057@skuniv.ac.kr>
2023-06-21 17:58:13 +0900

    #39 - 댓글 저장, 삭제 기능을 위한 컨트롤러와 서비스 코드 작성

---
+++ src/main/java/com/jsh/boardproject/dto/request/ArticleCommentRequest.java
@@ -0,0 +1,19 @@
+package com.jsh.boardproject.dto.request;
+
+import com.jsh.boardproject.dto.ArticleCommentDto;
+import com.jsh.boardproject.dto.UserAccountDto;
+
+public record ArticleCommentRequest(Long articleId, String content) {
+
+    public static ArticleCommentRequest of(Long articleId, String content){
+        return new ArticleCommentRequest(articleId,content);
+    }
+
+    public ArticleCommentDto toDto(UserAccountDto userAccountDto){
+        return ArticleCommentDto.of(
+                articleId,
+                userAccountDto,
+                content
+        );
+    }
+}
