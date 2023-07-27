3a27d60bbc92ee511c570342fba741ebce8af1f1
jsh9057 <jsh9057@skuniv.ac.kr>
2023-07-27 18:45:32 +0900

    #41 - 도메인 변경점 반영: 해시태그
    해시태그 엔티티가 추가되고, 게시글이 해시태그를 여러 개
    가질 수 있도록 설계를 변경함.
    이 때문에 변경점이 크게 전파되었다.
    커밋이 컴파일 에러 상태로 두는 것은 좋지 않아서
    어쩔 수 없이 변경점을 모두 한 커밋에 담는데
    이 방법이 괜찮은지 고민해보기.
    
    그러나 도메인 변경만 반영하고 기능을 구현하지 않았으므로
    컴파일 에러만 잠시 피할 뿐, 테스트는 현재 실패 함

---
+++ src/main/java/com/jsh/boardproject/service/HashtagService.java
@@ -0,0 +1,20 @@
+package com.jsh.boardproject.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+
+@Service
+public class HashtagService {
+    public Object parseHashtagNames(String content){
+        return null;
+    }
+
+    public Object findHashtagByNames(Set<String> expectedHashtagNaems){
+        return null;
+    }
+
+    public void deleteHashtagWithoutArticles(Object any){
+    }
+}
