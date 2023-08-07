c6b35b0e1ed18fa2262b25b22cc386ef00ce3401
jsh9057 <jsh9057@skuniv.ac.kr>
2023-08-07 22:25:58 +0900

    #21 - 테스트 코드의 누락된 테스트 이름 추가
    누락된 `@DisplayName` 에 적절한 이름을 추가 함

--- src/test/java/com/jsh/boardproject/controller/MainControllerTest.java
+++ src/test/java/com/jsh/boardproject/controller/MainControllerTest.java
@@ -2,6 +2,7 @@ package com.jsh.boardproject.controller;
 
 import com.jsh.boardproject.config.SecurityConfig;
 import com.jsh.boardproject.config.TestSecurityConfig;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -14,6 +15,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@DisplayName("View 루트 컨트롤러")
 @Import(TestSecurityConfig.class)
 @WebMvcTest(MainController.class)
 class MainControllerTest {
@@ -24,6 +26,7 @@ class MainControllerTest {
         this.mvc = mvc;
     }
 
+    @DisplayName("[view][GET] 루트페이지 -> 게시글 리스트 (게시판) 페이지 Redirection")
     @Test
     void givenNothing_whenRequestingRootPage_thenRedirectsToArticlesPage() throws Exception {
         // Given
