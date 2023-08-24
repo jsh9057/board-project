0b4b748dd791935f7b9622fcdc236c853c233a89
jsh9057 <jsh9057@skuniv.ac.kr>
2023-08-24 20:38:42 +0900

    #92 - 어드민 프로젝트에서 사용할 data rest api 들에 id 필드 오픈
    data rest가 자동 생성하는 api 는 id 값을 노출하지 않는 것이 기본 설정.
    이를 노출해준다.
    id 값은 어드민 프로젝트의 테이블에서 관리 목적으로 보여줄 예정.

--- src/main/java/com/jsh/boardproject/config/DataRestConfig.java
+++ src/main/java/com/jsh/boardproject/config/DataRestConfig.java
@@ -1,5 +1,8 @@
 package com.jsh.boardproject.config;
 
+import com.jsh.boardproject.domain.Article;
+import com.jsh.boardproject.domain.ArticleComment;
+import com.jsh.boardproject.domain.Hashtag;
 import com.jsh.boardproject.domain.UserAccount;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +14,11 @@ public class DataRestConfig {
     @Bean
     public RepositoryRestConfigurer repositoryRestConfigurer(){
         return RepositoryRestConfigurer.withConfig((config, cors) ->
-                config.exposeIdsFor(UserAccount.class)
+                config
+                        .exposeIdsFor(UserAccount.class)
+                        .exposeIdsFor(Article.class)
+                        .exposeIdsFor(ArticleComment.class)
+                        .exposeIdsFor(Hashtag.class)
         );
     }
 }
